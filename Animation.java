ef489e7147bdba37d0369ffa370b89866ca03d24
Kyle Krstulich <kylekrstulich@gmail.com>
2023-09-29 13:13:34 -0600

    Added standfords standard library folder with StdDraw. Added Animation.java skeleton file

---
+++ Animation.java
@@ -0,0 +1,18 @@
+/*
+ * Kyle Krstulich
+ * 9/29/23
+ * Animation.java
+ * 
+ * Please create a short (10 - 15 second) animation using the StdDraw library in Java.  Please use at least 3 different shapes, and image, text, and different colors.  Lastly, incorporate user interaction into your animation.
+ */
+
+import StdLib.StdDraw;
+
+public class Animation {
+
+    
+    public static void main(String[] args) {
+        
+    }
+
+}

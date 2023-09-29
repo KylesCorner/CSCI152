1d5558f14ef124f8d12b40c60ad058b3f5873d8b
Kyle Krstulich <kylekrstulich@gmail.com>
2023-09-29 13:35:16 -0600

    Refactored file structure for Std Libraries. Modified Animation.java

--- Animation.java
+++ Animation.java
@@ -6,13 +6,11 @@
  * Please create a short (10 - 15 second) animation using the StdDraw library in Java.  Please use at least 3 different shapes, and image, text, and different colors.  Lastly, incorporate user interaction into your animation.
  */
 
-import StdLib.StdDraw;
-
 public class Animation {
 
     
     public static void main(String[] args) {
-        
+        StdDraw.setCanvasSize(10, 10);
     }
 
 }
